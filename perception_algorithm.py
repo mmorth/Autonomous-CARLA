f1f42a5318553a010c1e8db7789527b8f20bf226
Matthew Orth <mmorth98@gmail.com>
2021-09-24 07:25:40 -0500

    Added working single-frame VO code.

--- perception_algorithm.py
+++ perception_algorithm.py
@@ -320,6 +320,207 @@ def draw_lanes(image_out, lane_lines):
     return image_out
 
 
+
+
+
+
+
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.ion()
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.mplot3d import Axes3D
+
+def extract_features(image):
+    orb = cv2.ORB_create()
+    kp, des = orb.detectAndCompute(image,None)
+    
+    return kp, des
+	
+
+def match_features(des1, des2):
+    bf = cv2.BFMatcher_create()
+    match = bf.knnMatch(des1,des2, k=2)  
+
+    # # FLANN parameters
+    # FLANN_INDEX_KDTREE = 1
+    # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    # search_params = dict(checks=50)   # or pass empty dictionary
+    
+    # flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+    # match = flann.knnMatch(des1,des2,k=2)
+
+    return match
+	
+	
+def filter_matches_distance(match, dist_threshold):
+    filtered_match = []
+    
+    for i,(m,n) in enumerate(match):
+        if m.distance < dist_threshold*n.distance:
+            filtered_match.append(m)
+
+    return filtered_match
+	
+
+def estimate_motion(match, kp1, kp2, k, depth1=None):
+    rmat = np.eye(3)
+    tvec = np.zeros((3, 1))
+    image1_points = []
+    image2_points = []
+
+    for m in match:
+        train_idx = m.trainIdx
+        query_idx = m.queryIdx
+        
+        p1x, p1y = kp1[query_idx].pt
+        image1_points.append([p1x, p1y])
+
+        p2x, p2y = kp2[train_idx].pt
+        image2_points.append([p2x, p2y])
+
+    E, mask = cv2.findEssentialMat(np.array(image1_points), np.array(image2_points), k)
+    
+    retval, rmat, tvec, mask = cv2.recoverPose(E, np.array(image1_points), np.array(image2_points), k)
+    
+    return rmat, tvec, image1_points, image2_points
+	
+	
+def estimate_trajectory(estimate_motion, match, kp1, kp2, k, depth):
+    trajectory = np.zeros((3, 1))
+    
+    trajectory = [np.array([0, 0, 0])]
+    P = np.eye(4)
+
+    rmat, tvec, image1_points, image2_points = estimate_motion(match, kp1, kp2, k, depth)
+    R = rmat
+    t = np.array([tvec[0,0],tvec[1,0],tvec[2,0]])
+    
+    P_new = np.eye(4)
+    P_new[0:3,0:3] = R.T
+    P_new[0:3,3] = (-R.T).dot(t)
+    P = P.dot(P_new)
+
+    trajectory.append(P[:3,3])
+
+    trajectory = np.array(trajectory).T   
+    trajectory[2,:] = -1*trajectory[2,:]
+    
+    return trajectory
+	
+	
+def visualize_trajectory(trajectory):
+    # Unpack X Y Z each trajectory point
+    locX = []
+    locY = []
+    locZ = []
+    # This values are required for keeping equal scale on each plot.
+    # matplotlib equal axis may be somewhat confusing in some situations because of its various scale on
+    # different axis on multiple plots
+    max = -math.inf
+    min = math.inf
+
+    # Needed for better visualisation
+    maxY = -math.inf
+    minY = math.inf
+
+    for i in range(0, trajectory.shape[1]):
+        current_pos = trajectory[:, i]
+        
+        locX.append(current_pos.item(0))
+        locY.append(current_pos.item(1))
+        locZ.append(current_pos.item(2))
+        if np.amax(current_pos) > max:
+            max = np.amax(current_pos)
+        if np.amin(current_pos) < min:
+            min = np.amin(current_pos)
+
+        if current_pos.item(1) > maxY:
+            maxY = current_pos.item(1)
+        if current_pos.item(1) < minY:
+            minY = current_pos.item(1)
+
+    auxY_line = locY[0] + locY[-1]
+    if max > 0 and min > 0:
+        minY = auxY_line - (max - min) / 2
+        maxY = auxY_line + (max - min) / 2
+    elif max < 0 and min < 0:
+        minY = auxY_line + (min - max) / 2
+        maxY = auxY_line - (min - max) / 2
+    else:
+        minY = auxY_line - (max - min) / 2
+        maxY = auxY_line + (max - min) / 2
+
+    # Set styles
+    mpl.rc("figure", facecolor="white")
+    plt.style.use("seaborn-whitegrid")
+
+    # Plot the figure
+    fig = plt.figure(0,figsize=(8, 6), dpi=100)
+    gspec = gridspec.GridSpec(3, 3)
+    ZY_plt = plt.subplot(gspec[0, 1:])
+    YX_plt = plt.subplot(gspec[1:, 0])
+    traj_main_plt = plt.subplot(gspec[1:, 1:])
+    D3_plt = plt.subplot(gspec[0, 0], projection='3d')
+
+    # Actual trajectory plotting ZX
+    toffset = 1.06
+    traj_main_plt.set_title("Autonomous vehicle trajectory (Z, X)", y=toffset)
+    traj_main_plt.set_title("Trajectory (Z, X)", y=1)
+    traj_main_plt.plot(locZ, locX, ".-", label="Trajectory", zorder=1, linewidth=1, markersize=4)
+    traj_main_plt.set_xlabel("Z")
+    # traj_main_plt.axes.yaxis.set_ticklabels([])
+    # Plot reference lines
+    traj_main_plt.plot([locZ[0], locZ[-1]], [locX[0], locX[-1]], "--", label="Auxiliary line", zorder=0, linewidth=1)
+    # Plot camera initial location
+    traj_main_plt.scatter([0], [0], s=8, c="red", label="Start location", zorder=2)
+    traj_main_plt.set_xlim([min, max])
+    traj_main_plt.set_ylim([min, max])
+    traj_main_plt.legend(loc=1, title="Legend", borderaxespad=0., fontsize="medium", frameon=True)
+
+    # Plot ZY
+    # ZY_plt.set_title("Z Y", y=toffset)
+    ZY_plt.set_ylabel("Y", labelpad=-4)
+    ZY_plt.axes.xaxis.set_ticklabels([])
+    ZY_plt.plot(locZ, locY, ".-", linewidth=1, markersize=4, zorder=0)
+    ZY_plt.plot([locZ[0], locZ[-1]], [(locY[0] + locY[-1]) / 2, (locY[0] + locY[-1]) / 2], "--", linewidth=1, zorder=1)
+    ZY_plt.scatter([0], [0], s=8, c="red", label="Start location", zorder=2)
+    ZY_plt.set_xlim([min, max])
+    ZY_plt.set_ylim([minY, maxY])
+
+    # Plot YX
+    # YX_plt.set_title("Y X", y=toffset)
+    YX_plt.set_ylabel("X")
+    YX_plt.set_xlabel("Y")
+    YX_plt.plot(locY, locX, ".-", linewidth=1, markersize=4, zorder=0)
+    YX_plt.plot([(locY[0] + locY[-1]) / 2, (locY[0] + locY[-1]) / 2], [locX[0], locX[-1]], "--", linewidth=1, zorder=1)
+    YX_plt.scatter([0], [0], s=8, c="red", label="Start location", zorder=2)
+    YX_plt.set_xlim([minY, maxY])
+    YX_plt.set_ylim([min, max])
+
+    # Plot 3D
+    D3_plt.set_title("3D trajectory", y=toffset)
+    D3_plt.plot3D(locX, locZ, locY, zorder=0)
+    D3_plt.scatter(0, 0, 0, s=8, c="red", zorder=1)
+    D3_plt.set_xlim3d(min, max)
+    D3_plt.set_ylim3d(min, max)
+    D3_plt.set_zlim3d(min, max)
+    D3_plt.tick_params(direction='out', pad=-2)
+    D3_plt.set_xlabel("X", labelpad=0)
+    D3_plt.set_ylabel("Z", labelpad=0)
+    D3_plt.set_zlabel("Y", labelpad=-2)
+    
+    # plt.axis('equal')
+    D3_plt.view_init(45, azim=30)
+
+    plt.tight_layout()
+    plt.show()
+    plt.pause(0.0001)
+
+
 # Visualizes predictions
 # Source: https://www.pyimagesearch.com/2021/08/02/pytorch-object-detection-with-pre-trained-networks/
 def visualize_predictions(orig, depth_img, ss_img, detections, fov):
@@ -386,7 +587,7 @@ def run_carla_client(args):
     with make_carla_client(args.host, args.port) as client:
         print('CarlaClient connected')
         
-        # TODO: Load object detection model
+        # Load object detection model
         model = torch.load(os.path.join(os.getcwd(), "models", 'model' + str(0) + '.pt'))
         model = model.cuda()
         print("Successfully loaded model!")
@@ -479,15 +680,40 @@ def run_carla_client(args):
                     img = to_bgra_array(measurement)
                     cur_img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
-                    # TODO: detect object locations using trained model
+                    # Detect object locations using trained model
                     transform = T.ToTensor()
                     input = transform(cur_img)[0]
                     img = input.unsqueeze_(0)
                     model.eval()
                     pred = model(input.cuda())[0]
-                    if depth_img is not None and ss_img is not None:
-                        depth_img = depth_img.reshape(depth_img.shape[0], depth_img.shape[1])
-                        visualize_predictions(cur_img, depth_img, ss_img, pred, camera_fov)
+                    # if depth_img is not None and ss_img is not None:
+                    #     depth_img = depth_img.reshape(depth_img.shape[0], depth_img.shape[1])
+                    #     visualize_predictions(cur_img, depth_img, ss_img, pred, camera_fov)
+
+                    if cur_img is not None and prev_img is not None:
+                        print("\nCOMPUTING VO!!!!!")
+                        # Part 1. Features Extraction
+                        cur_kp, cur_des = extract_features(cur_img)
+                        prev_kp, prev_des = extract_features(prev_img)
+
+                        # Part II. Feature Matching
+                        match = match_features(prev_des, cur_des)
+
+                        # Set to True if you want to use filtered matches or False otherwise
+                        is_main_filtered_m = True
+                        if is_main_filtered_m:
+                            dist_threshold = 0.75
+                            filtered_matches = filter_matches_distance(match, dist_threshold)
+                            match = filtered_matches
+
+                        # Part III. Trajectory Estimation
+                        k = np.array([[640, 0, 640],
+                                        [0, 480, 480],
+                                        [0,   0,   1]], dtype=np.float32)
+                        trajectory = estimate_trajectory(estimate_motion, match, prev_kp, cur_kp, k, depth_img)
+                        visualize_trajectory(trajectory)
+                    else:
+                        print("NOT COMPUTING VO")
 
                     prev_img = cur_img
                 elif name == 'CameraDepth':
